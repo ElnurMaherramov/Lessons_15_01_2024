746e11fc279becd6bb198647d0ed000bfb4f54de
Elnur <magerramovelnur94@gmail.com>
2024-01-25 19:05:47 +0200

    Homework 3, if/elif/else, list

--- Homework3.py
+++ Homework3.py
@@ -23,17 +23,20 @@ value_list_2 = [1]
 value_list_3 = []
 value_list_4 = [12, 3, 4, 10, 8]
 
-value_list_1.insert(0, 10)
-value_list_2.insert(0, 1)
-value_list_3.insert(0, '')
-value_list_4.insert(0, 8)
+if value_list_1:
+    value_list_1.insert(0, value_list_1[-1])
+    value_list_1.pop()
 
-value_list_1.pop()
-value_list_2.pop()
-value_list_3.pop()
-value_list_4.pop()
+if value_list_2:
+    value_list_2.insert(0, value_list_2[-1])
+    value_list_2.pop()
 
-print(value_list_1)
-print(value_list_2)
-print(value_list_3)
-print(value_list_4)
+if value_list_3:
+    value_list_3.insert(0, value_list_3[-1])
+    value_list_3.pop()
+
+if value_list_4:
+    value_list_4.insert(0, value_list_4[-1])
+    value_list_4.pop()
+
+print('\n', value_list_1, '\n', value_list_2, '\n', value_list_3, '\n', value_list_4)
