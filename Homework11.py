964948fbfbefee18b93e1a9d03edb735f05e3bb8
Elnur <magerramovelnur94@gmail.com>
2024-02-25 23:13:03 +0200

    Homework 11

---
+++ Homework11.py
@@ -0,0 +1,33 @@
+### Завдання 1 Генератор простих чисел
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def prime_generator(end):
+    current_num = 2
+    while current_num <= end:
+        if is_prime(current_num):
+            yield current_num
+        current_num += 1
+
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
+
+### Завдання 2 Перевірка на парність.
+def is_even(number):
+    return (number & 1) == 0
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('Ok')
